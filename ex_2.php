f641e6f81b23865f23da030fc2fcdb08b3097c18
José Flora <jmrfloraxbox@gmail.com>
2020-08-18 10:41:19 -0300

    Create ex_2.php

---
+++ ex_2.php
@@ -0,0 +1,39 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Document</title>
+</head>
+<body>
+	
+<form method="get" >
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+
+  <label for="date">Data de nascimento:</label><br>
+  <input type="number" id="date" name="date" max="2019"><br>
+
+  <input type="submit" value="concluir">
+</form>
+	
+<?php
+if(isset($_GET['fname']))
+{
+
+$nome = $_GET['fname'];
+$data = $_GET['date'];
+
+//echo $nome."<br>";
+//echo $data;
+
+$num =(int)$data;
+
+$idade = 2020-($num);
+
+echo "<br>sua idade:".$idade;
+}
+
+?>
+</body>
+</html>
