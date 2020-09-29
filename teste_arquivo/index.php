7ee0a9dfc669627e0686b605653172ce8685a6c2
José Flora <jmrfloraxbox@gmail.com>
2020-09-29 10:07:02 -0300

    Create index.php

---
+++ teste_arquivo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="cria_arquivo.php" method="post">
+      <label>matricula</label><br>
+      <input type="text" name="matricula" required><br><br>
+      <label>nome</label><br>
+      <input type="text" name="nome" value="" required><br><br>
+      <label>endereço</label><br>
+      <input type="text" name="endereço" value="" required><br><br>
+      <label>cep</label><br>
+      <input type="text" name="cep" value="" required><br><br>
+      <label>cidade</label><br>
+      <input type="text" name="cidade" value="" required><br><br>
+      <label>estado</label><br>
+      <input type="text" name="estado" value="" required><br><br>
+      <label>cpf</label><br>
+      <input type="text" name="cpf" value="" required><br><br>
+      <label>data de nascimento</label><br><br>
+      <input type="date" name="nascimento" value="" required><br><br>
+      <input type="submit">
+    </form>
+  </body>
+</html>
